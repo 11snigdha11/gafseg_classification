6d3867027c6d0a605da76078a8eaf6fd5010fc3f
11snigdha11 <snigdha.p2004@gmail.com>
2026-06-12 15:09:41 +0530

    softmax

--- attacks/naive.py
+++ attacks/naive.py
@@ -72,44 +72,44 @@ def random_attack_model(local_model, global_model):
 
 
 
-# def gaussian_attack_model(local_model, global_model, sigma=0.1):
+def gaussian_attack_model(local_model, global_model, sigma=0.1):
 
-#     local_dict = local_model.state_dict()
-#     global_dict = global_model.state_dict()
-
-#     attacked_dict = {}
-
-#     for k in global_dict.keys():
-
-#         update = local_dict[k].float() - global_dict[k].float()
-
-#         noise = sigma * torch.randn_like(update)
-
-#         attacked_dict[k] = (
-#             global_dict[k].float() + update + noise
-#         )
-
-#     local_model.load_state_dict(attacked_dict)
-
-#     return local_model    
-
-
-def gaussian_attack_model(local_model, global_model, sigma=10.0):
     local_dict = local_model.state_dict()
     global_dict = global_model.state_dict()
+
     attacked_dict = {}
 
     for k in global_dict.keys():
-        # Do not corrupt BatchNorm statistics (prevents the NaN-Bomb)
-        if 'running' in k or 'num_batches_tracked' in k:
-            attacked_dict[k] = global_dict[k].float()
-        else:
-            # PURE NOISE ATTACK: Completely replace the legitimate update
-            # We use a large sigma to ensure the vector is fully orthogonal
-            noise = sigma * torch.randn_like(global_dict[k].float())
-            
-            # Attacker sends: Base Global Model + Pure Noise
-            attacked_dict[k] = global_dict[k].float() + noise
+
+        update = local_dict[k].float() - global_dict[k].float()
+
+        noise = sigma * torch.randn_like(update)
+
+        attacked_dict[k] = (
+            global_dict[k].float() + update + noise
+        )
 
     local_model.load_state_dict(attacked_dict)
-    return local_model
+
+    return local_model    
+
+
+# def gaussian_attack_model(local_model, global_model, sigma=10.0):
+#     local_dict = local_model.state_dict()
+#     global_dict = global_model.state_dict()
+#     attacked_dict = {}
+
+#     for k in global_dict.keys():
+#         # Do not corrupt BatchNorm statistics (prevents the NaN-Bomb)
+#         if 'running' in k or 'num_batches_tracked' in k:
+#             attacked_dict[k] = global_dict[k].float()
+#         else:
+#             # PURE NOISE ATTACK: Completely replace the legitimate update
+#             # We use a large sigma to ensure the vector is fully orthogonal
+#             noise = sigma * torch.randn_like(global_dict[k].float())
+            
+#             # Attacker sends: Base Global Model + Pure Noise
+#             attacked_dict[k] = global_dict[k].float() + noise
+
+#     local_model.load_state_dict(attacked_dict)
+#     return local_model
