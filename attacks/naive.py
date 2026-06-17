b5e71e0227fde4303c000ef911f291cba78a198f
11snigdha11 <snigdha.p2004@gmail.com>
2026-06-17 16:12:52 +0530

    softmax signflip

--- attacks/naive.py
+++ attacks/naive.py
@@ -3,24 +3,40 @@ import torch
 
 
 
-def signflip_attack_model(local_model, global_model):
+# def signflip_attack_model(local_model, global_model):
 
+#     local_dict = local_model.state_dict()
+#     global_dict = global_model.state_dict()
+
+#     attacked_dict = {}
+
+#     for k in global_dict.keys():
+
+#         update = local_dict[k].float() - global_dict[k].float()
+
+#         attacked_dict[k] = (
+#             global_dict[k].float() - update
+#         )
+
+#     local_model.load_state_dict(attacked_dict)
+
+#     return local_model
+def signflip_attack_model(local_model, global_model):
     local_dict = local_model.state_dict()
     global_dict = global_model.state_dict()
-
     attacked_dict = {}
 
     for k in global_dict.keys():
-
-        update = local_dict[k].float() - global_dict[k].float()
-
-        attacked_dict[k] = (
-            global_dict[k].float() - update
-        )
+        # Prevent structural NaN-bomb crashes
+        if 'running' in k or 'num_batches_tracked' in k:
+            attacked_dict[k] = local_dict[k].float()
+        else:
+            # Apply Sign-Flip to the actual learning weights/biases
+            update = local_dict[k].float() - global_dict[k].float()
+            attacked_dict[k] = global_dict[k].float() - update
 
     local_model.load_state_dict(attacked_dict)
-
-    return local_model
+    return local_model    
 def scaling_attack_model(local_model, global_model, factor=20):
 
     local_dict = local_model.state_dict()
