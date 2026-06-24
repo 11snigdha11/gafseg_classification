9ad0a681711fc47162ad743d1eae917a00f81be8
11snigdha11 <snigdha.p2004@gmail.com>
2026-06-24 10:29:34 +0530

    with mu and fc(final)

--- attacks/naive.py
+++ attacks/naive.py
@@ -2,25 +2,6 @@ import copy
 import torch
 
 
-
-# def signflip_attack_model(local_model, global_model):
-
-#     local_dict = local_model.state_dict()
-#     global_dict = global_model.state_dict()
-
-#     attacked_dict = {}
-
-#     for k in global_dict.keys():
-
-#         update = local_dict[k].float() - global_dict[k].float()
-
-#         attacked_dict[k] = (
-#             global_dict[k].float() - update
-#         )
-
-#     local_model.load_state_dict(attacked_dict)
-
-#     return local_model
 def signflip_attack_model(local_model, global_model):
     local_dict = local_model.state_dict()
     global_dict = global_model.state_dict()
@@ -36,7 +17,8 @@ def signflip_attack_model(local_model, global_model):
             attacked_dict[k] = global_dict[k].float() - update
 
     local_model.load_state_dict(attacked_dict)
-    return local_model    
+    return local_model  
+
 def scaling_attack_model(local_model, global_model, factor=20):
 
     local_dict = local_model.state_dict()
@@ -65,49 +47,43 @@ def random_attack_model(local_model, global_model):
     attacked_dict = {}
 
     for k in global_dict.keys():
-
-        update = local_dict[k].float() - global_dict[k].float()
-
-        update_norm = torch.norm(update)
-
-        random_update = torch.randn_like(update)
-
-        random_update = (
-            random_update
-            / (torch.norm(random_update) + 1e-12)
-            * update_norm
-        )
-
-        attacked_dict[k] = (
-            global_dict[k].float() + random_update
-        )
+        if 'running' in k or 'num_batches_tracked' in k:
+            attacked_dict[k] = local_dict[k].float()
+        else:
+            update = local_dict[k].float() - global_dict[k].float()
+            update_norm = torch.norm(update)
+            random_update = torch.randn_like(update)
+            # Scale random noise to match the magnitude of the original update
+            random_update = (random_update / (torch.norm(random_update) + 1e-12)) * update_norm
+            attacked_dict[k] = global_dict[k].float() + random_update
 
     local_model.load_state_dict(attacked_dict)
-
-    return local_model    
-
+    return local_model
 
 
-# def gaussian_attack_model(local_model, global_model, sigma=0.1):
 
-#     local_dict = local_model.state_dict()
-#     global_dict = global_model.state_dict()
 
-#     attacked_dict = {}
 
-#     for k in global_dict.keys():
+def additive_gaussian_attack_model(local_model, global_model, sigma=0.1):
+    local_dict = local_model.state_dict()
+    global_dict = global_model.state_dict()
+    attacked_dict = {}
 
-#         update = local_dict[k].float() - global_dict[k].float()
+    for k in global_dict.keys():
+        if 'running' in k or 'num_batches_tracked' in k:
+            attacked_dict[k] = local_dict[k].float()
+        else:
+            # 1. Calculate the legitimate update
+            update = local_dict[k].float() - global_dict[k].float()
+            
+            # 2. Generate small additive noise
+            noise = sigma * torch.randn_like(update)
+            
+            # 3. Attacker sends: Base Global Model + Real Update + Small Noise
+            attacked_dict[k] = global_dict[k].float() + update + noise
 
-#         noise = sigma * torch.randn_like(update)
-
-#         attacked_dict[k] = (
-#             global_dict[k].float() + update + noise
-#         )
-
-#     local_model.load_state_dict(attacked_dict)
-
-#     return local_model    
+    local_model.load_state_dict(attacked_dict)
+    return local_model   
 
 
 def gaussian_attack_model(local_model, global_model, sigma=10.0):
